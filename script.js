1fd6dd818e9c4df4f208b910cf1a40078a438ec3
Hugo <djgodg@gmail.com>
2024-03-29 00:09:33 +0100

    push

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Import styles */
+import "./styles_v2024.css";
+
+/* Import locomotive-scroll */
+import "./locomotive-scroll.css";
+import LocomotiveScroll from "locomotive-scroll";
+
+/* Locomotive scroll instance */
+const locomotiveScroll = new LocomotiveScroll();
